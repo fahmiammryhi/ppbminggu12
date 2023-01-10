9e1e2b5b802441252feb7bd45050311695f3749c
BoyTrek <117712317+BoyTrek@users.noreply.github.com>
2023-01-10 15:28:14 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,104 +1,106 @@
-import React, { Component } from 'react';
-import { NativeBaseProvider, Icon, Text} from 'native-base' ; 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
-import { Navigationcontainer } from '@react-navigation/native'; 
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
-import Home from './page/home'; 
-import News from './page/NewsDetails.js';
-import Profil from './page/about';
+import React from 'react';
+import { NativeBaseProvider, Icon, Text } from 'native-base';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+import Home from './components/home';
+import News from './components/NewsDetails';
+import Profil from './components/About';
 
-
-const Tab = createBottomTabNavigator(); 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const BottomNavigator = () => {
   return (
-    <Tab.Navigator 
-    screenoptions={({ route }) => ({ 
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName; 
-        if (route.name === 'Home' ) { 
-          iconName = 'home-variant-outline';
-         } else if (route.name === 'About') { 
-           iconName = ' information-variant '
-         }
-         return (
-            <Icon 
-            as={MaterialCommunityIcons}
-            name={iconName}
-            size={30}
-          	color={focused ? 'primary.600' : '#6E8FAD'}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home-variant-outline';
+          } else if (route.name === 'Praktikum') {
+            iconName = 'clipboard-list';
+          } else if (route.name === 'About') {
+            iconName = 'information-variant';
+          }
+          return (
+            <Icon
+              as={MaterialCommunityIcons}
+              name={iconName}
+              size={30}
+              color={focused ? 'primary.600' : '#6E8FAD'}
             />
-            );
-           },
-          tabBarIconStyle : { marginTop: 10 }, 
-          tabBarLabel: ({ children, color, focused } ) => { 
-            return (
-	        <Text color={focused ? 'primary.600' : color} mb={2}>
-          {children}
-          </Text>
-            );  
-          },
-        tabBarStyle: { 
+          );
+        },
+        tabBarIconStyle: { marginTop: 10 },
+        tabBarLabel: ({ children, color, focused }) => {
+          return (
+            <Text color={focused ? 'primary.600' : color} mb={2}>
+              {children}
+            </Text>
+          );
+        },
+        tabBarStyle: {
           height: 50,
           borderTopWidth: 0,
         },
       })}>
       <Tab.Screen
-        name='Home'
-        Component={Home}
+        name="Home"
+        component={Home}
         options={{
-        unmountOnBlur: true,
-        headerShown: false,
-        tabBarLabel: () => {
-         return null;
-        },
+          unmountOnBlur: true,
+          headerShown: false,
+          tabBarLabel: () => {
+            return null;
+          },
         }}
       />
-     <Tab.Screen
-        name='About'
-        Component={Profil}
+      <Tab.Screen
+        name="About"
+        component={Profil}
         options={{
-        unmountOnBlur: true,
-        headerShown: false,
-        tabBarLabel: () => {
-         return null;
-        },
-      }}
+          unmountOnBlur: true,
+          headerShown: false,
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
       />
     </Tab.Navigator>
   );
 };
 
 const App = () => {
-      return (
-        <NativeBaseProvider>
-        <Navigationcontainer>
+  return (
+    <NativeBaseProvider>
+      <NavigationContainer>
         <Stack.Navigator>
-        <Stack.Screen
-          name="BottomNavigator"
-          Component={BottomNavigator}
-          options={{ headerShown: false }}
+          <Stack.Screen
+            name="BottomNavigator"
+            component={BottomNavigator}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
-          name="NewsDetail"
-          Component={News}
-          options={{
-            tittle: 'Detail Berita',
-            headerStyle: {
-              backgroundcolor: '#52ACFF',
-            },
-            headerTintColor: '#fff',
-            headerTintStyle: {
-              fontWeight: 'bold',
-            },
-          }}
+            name="NewsDetail"
+            component={News}
+            options={{
+              title: 'Detail Berita',
+              headerStyle: {
+                backgroundColor: '#52ACFF',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
           />
         </Stack.Navigator>
-      </Navigationcontainer>
-      </NativeBaseProvider>
-      );
-    };
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+};
 
-        export default App;
+export default App;
