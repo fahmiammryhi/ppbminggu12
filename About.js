542c1ab3b23e730e0657913e329d59ae1f846cb0
ilyasky19 <102515986+ilyasky19@users.noreply.github.com>
2023-01-10 14:36:55 +0700

    Create About.js

---
+++ About.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  ScrollView,
+  Text,
+  VStack,
+  Center,
+  AlertDialog,
+} from 'native-base';
+
+class Profil extends React.Component {
+  render() {
+    return (
+      <Box flex={1}>
+      <ScrollView px={3} py={5}>
+      <Box>
+      <Heading>App Description</Heading>
+      </Text>
+      </Box>
+      <Divider mt="20px" mb="15px" />
+      <Box>
+      <Heading mb="10px">App Info</Heading>
+      <VStack>
+       <HStack justifycontent={'space-between'}>
+       <Text fontweight={'bold'}>Version</Text>
+       <Text>v0.0.1</Text>
+       </HStack>
+       <HStack justifycontent={'space-between'}>
+       <Text fontweight={'bold'}>Update on</Text>
+       <Text>January 10, 2023</Text>
+       </HStack>
+       </VStack>
+       </Box>
+       <Divider mt="20px" mb="15px" />
+       </ScrollView>
+       </Box>
+    );
+  }
+}
+
+export default Profil;
